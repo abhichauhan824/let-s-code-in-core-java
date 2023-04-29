0bd12eb53985230c56b021048a6293fe7505829e
abhichauhan824 <94954660+abhichauhan824@users.noreply.github.com>
2023-04-29 11:32:57 +0530

    Create Hello .java
    First program in core java programming .
     In which we simply Print the Hello Duniya with the help of Print statement .

---
+++ Hello .java
@@ -0,0 +1,10 @@
+# let-s-code-in-core-java
+In this repository we only code in java
+
+import java.io.*.;
+
+class First{
+      public static void main(String a[]){
+             System.out.println("Hello Duniya..!");
+      }
+}
