6a2b0293ee693e187d7419ef2b76b0d8151b057d
Abhishek Singh <94954660+abhichauhan824@users.noreply.github.com>
2023-05-20 11:13:06 +0530

    Create stu.java
    Take a input from the user about his information by the help of util package.

---
+++ stu.java
@@ -0,0 +1,26 @@
+
+#import java.util.*;
+
+class Stu{
+      public static void main(String a[]){
+            Scanner sc = new Scanner(System.in);
+        
+            System.out.print("Enter your name : ");
+            String str = sc.nextLine();
+        
+            System.out.print("Enter your course : ");
+            String str1 = sc.nextLine();
+        
+            System.out.print("Enter your mobile number : ");
+            int num = sc.nextInt();
+        
+            System.out.println(" your name is : " + str);
+           
+        
+            System.out.println(" your course is : "+ str1);
+           
+        
+            System.out.print(" your mobile number is : " + num);
+            
+      }
+}
